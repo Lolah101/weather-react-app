d2977c9968a2c136c444f75058a5dc7a2edcd2d1
Loza <97474199+Lolah101@users.noreply.github.com>
2023-08-19 07:57:50 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/Lolah101/weather-react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Open-source
+        </a>{" "}
+        code by{" "}
+        <a
+          href="https://www.linkedin.com/in/loza-d-a1a947252/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Loza Daniel
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-
